6992f8ed24315c1dc8b78cab7ec108d66a8d7eb5
ChaosNyaruko <cabbageparadise@gmail.com>
2022-04-07 17:56:31 +0800

    small refactor, wrapping git operations in dependent functions, passing path as params.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -10,11 +11,11 @@ import (
 // TODO: define my own var to implement list values (e.g. -o dotfile dollop )
 // check https://pkg.go.dev/flag#Value.
 var obj = flag.String("o", "./tmp", "where you want to launch the sync process")
-var push = flag.Bool("p", false, "whether automatically push commit to remote")
+var p = flag.Bool("p", false, "whether automatically push commit to remote")
 
 func main() {
 	flag.Parse()
-	log.Println(*push)
+	log.Println(*p)
 	// var objs  = []string{}
 	var dotfilesPath string
 	if home, err := os.UserHomeDir(); err != nil {
@@ -23,33 +24,53 @@ func main() {
 		dotfilesPath = home + "/GitPrjs/dotfiles"
 		log.Println(home, dotfilesPath)
 	}
+
+	if err := pull(dotfilesPath); err != nil {
+		log.Println(err)
+	}
+	if err := commit(dotfilesPath); err != nil {
+		log.Println(err)
+	}
+	if *p {
+		if err := push(dotfilesPath); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func pull(path string) error {
+	log.Println("git pulling..")
 	pull := exec.Command("git", "pull", "origin", "main")
-	pull.Dir = dotfilesPath
+	pull.Dir = path
 	pull.Stdout = os.Stdout
 	pull.Stderr = os.Stderr
-	log.Println("git pulling..")
 	if err := pull.Run(); err != nil {
-		log.Fatalf("git pull err: '%v'", err)
+		return fmt.Errorf("git pull err: %v '%v'", pull, err)
 	}
+	return nil
+}
+
+func commit(path string) error {
 	log.Println("committing..")
 	commit := exec.Command("git", "commit", "-a")
 	// TODO: specify the commit msg without launch editor
-	log.Println(commit)
-	commit.Dir = dotfilesPath
+	commit.Dir = path
 	commit.Stdout = os.Stdout
 	commit.Stderr = os.Stderr
 	if err := commit.Run(); err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("git commit err: %v '%v'", commit, err)
 	}
-	if *push {
-		log.Println("pushing..")
-		push := exec.Command("git", "push")
-		log.Println(push)
-		push.Dir = dotfilesPath
-		push.Stdout = os.Stdout
-		push.Stderr = os.Stderr
-		if err := push.Run(); err != nil {
-			log.Fatal(err)
-		}
+	return nil
+}
+
+func push(path string) error {
+	log.Println("pushing..")
+	push := exec.Command("git", "push")
+	push.Dir = path
+	push.Stdout = os.Stdout
+	push.Stderr = os.Stderr
+	if err := push.Run(); err != nil {
+		return fmt.Errorf("git push err: %v '%v'", push, err)
 	}
+	return nil
 }
